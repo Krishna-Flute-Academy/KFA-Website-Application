6f0fb70cc516ae5846e127cec7f4730019b2a105
Pranshu Dev <23BTRCL091@jainuniversity.ac.in>
2026-06-26 22:27:24 +0530

    Push-Notifications

--- app/student-dashboard/page.tsx
+++ app/student-dashboard/page.tsx
@@ -111,6 +111,7 @@ export default function StudentDashboard() {
                 OneSignal.push(() => {
                     OneSignal.init({
                         appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID || "",
+                        safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_WEB_ID || undefined,
                         allowLocalhostAsSecureOrigin: true,
                         notifyButton: {
                             enable: false,
